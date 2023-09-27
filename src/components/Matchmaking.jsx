af2da37552048081905df2f918b06fb82c863f57
SachinDevelopment <53714757+SachinDevelopment@users.noreply.github.com>
2023-09-27 19:15:09 -0400

    style champs

--- src/components/Matchmaking.jsx
+++ src/components/Matchmaking.jsx
@@ -1,8 +1,30 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
+const players = [
+  { player: "sachin", champ: "" },
+  { player: "parth", champ: "" },
+  { player: "jonah", champ: "" },
+];
 const Matchmaking = () => {
   const [champ, setChamp] = useState([]);
+  const [blueTeam, setBlueTeam] = useState(players);
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    //Implementing the setInterval method
+    const interval = setInterval(() => {
+      if (count <= 30) {
+        setCount(count + 1);
+      } else {
+        setCount(0);
+      }
+    }, 1000);
+
+    //Clearing the interval
+    return () => clearInterval(interval);
+  }, [count]);
 
   useEffect(() => {
     (async () => {
@@ -14,11 +36,12 @@ const Matchmaking = () => {
   }, [setChamp]);
 
   return (
-    <div className="w-3/4">
-      <div className="grid grid-cols-10 gap-4">
+    <div className="w-3/4 flex justify-center">
+      <div className="grid grid-cols-10 gap-4 w-50v overflow-auto h-70v">
         {champ.map((c) => (
           <img
             src={`http://ddragon.leagueoflegends.com/cdn/13.19.1/img/champion/${c.id}.png`}
+            // onClick={() => setBlueTeam((prev) => [...prev, c])}
           />
         ))}
       </div>
